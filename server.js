ac32d9d9ec8aa43a923cbc41cacf3966f5ae911c
Shaharia hasan <hshaharia064@gmail.com>
2026-05-31 02:15:58 +0530

    file system in node js

--- server.js
+++ server.js
@@ -1,26 +1,8 @@
 import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-import { User } from "./models/user-models.js";
 
 const app = express();
 
-// middlewares
-app.use(cors());
-app.use(express.json());
-app.use(express.static("public"));
-
-// db connection
-mongoose
-  .connect("mongodb://127.0.0.1:27017/users_demo")
-  .then(() => console.log("Mongoose connected"));
-
-//   Routes
-app.get("/api/users", async (req, res) => {
-  const users = await User.find();
-  res.status(200).json({ data: users });
-});
-
+// server start
 app.listen(5000, () => {
   console.log("Express is running...");
 });
